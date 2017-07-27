56f9612b0c92f2925d7f362e51a4f8deb5f2c442
keisuke2236 <terada@basicinc.jp>
2017-07-27 16:01:40 +0900

    不要なファイル削除とファイル名の変更

---
+++ sabaku.rb
@@ -0,0 +1,16 @@
+# rails server
+
+include_recipe "selinux::disabled"
+include_recipe "cookbooks/yum/default.rb"
+include_recipe "cookbooks/iptables/default.rb"
+include_recipe "cookbooks/etc_packages/default.rb"
+include_recipe "cookbooks/ntp/default.rb"
+
+include_recipe 'rtn_rbenv::user'
+
+if node[:env] != 'development'
+  include_recipe "cookbooks/nginx/default.rb"
+  include_recipe "cookbooks/unicorn/default.rb"
+end
+
+include_recipe "cookbooks/mysql/default.rb"
